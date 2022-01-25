76b2d9eec405e88a4774ef81c9d5f337fd5dc193
Brett Clark <bwclark@sandia.gov>
2022-01-25 12:35:10 -0700

    Adding logic to switch between esp 1.17 and 1.20

--- var/spack/repos/plato/packages/esp/package.py
+++ var/spack/repos/plato/packages/esp/package.py
@@ -8,7 +8,7 @@ class Esp(Package):
     url      = "https://acdl.mit.edu/ESP/PreBuilts/ESP120Lin.tgz"
 
     version('120Lin', sha256='d16c7d90d4e1b46973113e47f474b59057df35f9bd55680c3430aface1571ba9', url='https://acdl.mit.edu/ESP/PreBuilts/ESP120Lin.tgz')
-    version('117Lin', sha256='bd6418ee9dafabdc17c58449c379535f4f148f1f67730074297c605b5e10e1a0', url='https://acdl.mit.edu/ESP/archive/ESP117Lin.tgz')
+    version('117Lin', sha256='bd6418ee9dafabdc17c58449c379535f4f148f1f67730074297c605b5e10e1a0', url='https://acdl.mit.edu/ESP/archive/ESP117Lin.tgz', preferred=True)
 
     depends_on( 'python@2.6:2.999', type=('run'), when='@117Lin' )
     depends_on( 'python@3.8:3.999', type=('run'), when='@120Lin' )
@@ -37,11 +37,13 @@ class Esp(Package):
 
     def setup_environment(self, spack_env, run_env):
 
-      run_env.prepend_path('PYTHONPATH', self.prefix.pyESP)
+      if (self.spec.satisfies('@120Lin')):
+        run_env.prepend_path('PYTHONPATH', self.prefix.pyESP)
       run_env.prepend_path('PYTHONPATH', self.prefix.lib)
       run_env.set('ESP_START', 'google-chrome '+self.prefix.ESP+'/ESP-localhost7681.html')
       run_env.set('UDUNITS2_XML_PATH', self.prefix+'/src/CAPS/udunits/udunits2.xml')
       run_env.set('ESP_ROOT', self.prefix)
-      run_env.set('ESP_ARCH', 'LINUX64')
-      run_env.set('CASREV', '7.4')
+      if (self.spec.satisfies('@120Lin')):
+        run_env.set('ESP_ARCH', 'LINUX64')
+        run_env.set('CASREV', '7.4')
 
